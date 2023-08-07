7a8822562e6f10a10cf413cfc80f8547af729a23
kimsegong <kimsegong@daum.net>
2023-08-07 16:53:37 +0900

    20230807

--- 20_Network_Ex/src/ex02_json_file/MainWrapper.java
+++ 20_Network_Ex/src/ex02_json_file/MainWrapper.java
@@ -1,11 +1,21 @@
 package ex02_json_file;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+
+
 public class MainWrapper {
 
-  
-  public static void main(String[] args) {
-    
-    // 문제. 아래 내용을 가지는 product.json 파일 생성하기
+  public static void ex01() {
+ // 문제. 아래 내용을 가지는 product.json 파일 생성하기
     /*
         [ 
           {
@@ -26,7 +36,55 @@ public class MainWrapper {
              ]
             }
            ]
-     */
+     */    
+    Map<String, Object> map = new HashMap<String, Object>();
+    map.put("code", "0001");
+    map.put("name", "냉장고");
+    map.put("price", "100");
+    map.put("sales", Arrays.asList("하이마트", "전자마트"));
+    
+    Map<String, Object> map1 = new HashMap<String, Object>();
+    map1.put("code", "0002");
+    map1.put("name", "에어컨");
+    map1.put("price", "200");
+    map1.put("sales", "하이마트");
+    
+    List<Map<String, Object>> list = Arrays.asList(map, map1);
+
+    JSONArray array = new JSONArray(list);
+    
+    // JSONArray -> String
+    String products =array.toString();
+    
+    // 디렉터리 File 객체
+    File dir = new File("C:/storage");    
+    if(dir.exists() == false) {
+      dir.mkdirs();
+    }
+    File file = new File(dir, "product.json");
+    
+    // try - catch -resources (close를 생략할 수 있는 try 문)
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+      bw.write(products);
+      System.out.println(file.getPath() + "파일 생성 완료");
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
+ public static void ex02() {
+     
+    // 문제. 아래 링크에 연결된 JSON 파일을 모두 읽은 뒤 아래 데이터를 가져와서 출력하시오.
+    // 가져올 데이터)
+    // ○ (강수) 10일(목) 전국에, 11일(금) 오전 수도권과 강원도에 비가 오겠습니다.<br />○ (기온) 이번 예보기간 아침 기온은 23~26도, 낮 기온은 27~33도로 평년(최저기온 22~24도, 최고기온 29~32도)과 비슷하겠습니다. <br />○ (해상) 전해상에 10일(목)까지, 동해상은 11일(금)까지 물결이 1.0~4.0m(동해상과 남해동부해상 5.0m 이상)로 매우 높게 일겠습니다.<br />○ (주말전망) 12일(토)과 13일(일)은 전국이 구름많겠습니다. 아침 기온은 23~26도, 낮 기온은 30~33도가 되겠습니다.
+
+    String spec = "https://gdlms.cafe24.com/uflist/curri/10014/bbs/231_64d09afdc7c01.json";
+    
+  }
+  
+  
+  public static void main(String[] args) {
+    
+    
   }
 
 }
---
+++ 20_Network_Ex/src/ex03_api/MainWrapper.java
@@ -0,0 +1,81 @@
+package ex03_api;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    // 문제. data.go.kr에서 "한국전력공사_전기차 충전소 운영정보" API를 신청하고
+    // 결과를 "C:storage"/한국전력공사_전기차_충전소_운영정보.xml 저장하시오.
+    
+    
+    URL url = null;
+    HttpURLConnection con = null;
+    BufferedReader reader = null;
+    BufferedWriter writer = null;
+    
+    
+    try {
+      String spec = "http://openapi.kepco.co.kr/service/EvInfoServiceV2/getEvSearchList";
+      String serviceKey ="RoB3A0x7JpkjrGS0+9iFdFEb6dOUVIdd+psO4rNbW//41vDD/ZB7P84WIJx45yN6SJVbtpt9Ws5FOECOOPLrZg==";
+      String pageNo = "1";
+      String numOfRows = "10";
+      String addr = "전라남도 나주시 빛가람동 120";
+      
+      StringBuilder sb = new StringBuilder();
+      sb.append(spec);
+      sb.append("?serviceKey=").append(URLEncoder.encode(serviceKey, "UTF-8"));
+      sb.append("&pageNo=").append(URLEncoder.encode(pageNo, "UTF-8"));
+      sb.append("&numOfRows=").append(URLEncoder.encode(numOfRows, "UTF-8"));
+      sb.append("&addr=").append(URLEncoder.encode(addr, "UTF-8"));
+      
+      url = new URL(sb.toString());
+      con = (HttpURLConnection)url.openConnection();
+      int response = con.getResponseCode();
+      if(con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+        throw new RuntimeException(response + "에러발생");
+      }
+      reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      
+      StringBuilder sbuf = new StringBuilder();
+      String line = null;
+      while((line = reader.readLine()) != null) {
+        sbuf.append(line + "\n");
+      }
+      
+      File dir = new File("C:/storage");
+      if(dir.exists() == false) {
+        dir.mkdirs();
+      }
+      File file = new File(dir, "한국전력공사_전기차_충전소_운영정보.xml");
+      writer = new BufferedWriter(new FileWriter(file));
+      writer.write(sbuf.toString());
+      System.out.println(file.getPath() + "파일 생성 완료");
+    } catch(UnsupportedEncodingException e) {
+      System.out.println(e.getMessage());
+    } catch(MalformedURLException e) {
+      System.out.println(e.getMessage());
+    } catch(IOException | RuntimeException e) { 
+      System.out.println(e.getMessage());
+    } finally {
+      try {
+        if(writer != null) writer.close();
+        if(reader != null) reader.close();
+        if(con != null) con.disconnect();        
+      }catch(IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
