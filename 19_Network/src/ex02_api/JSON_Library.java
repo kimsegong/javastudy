4aab9f26ec5d14a09e48f0d4c5f45f88f688e17a
kimsegong <kimsegong@daum.net>
2023-08-07 15:00:19 +0900

    20230807

--- 19_Network/src/ex02_api/JSON_Library.java
+++ 19_Network/src/ex02_api/JSON_Library.java
@@ -1,6 +1,5 @@
 package ex02_api;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
--- 20_Network_Ex/src/ex02_json_file/MainWrapper.java
+++ 20_Network_Ex/src/ex02_json_file/MainWrapper.java
@@ -2,9 +2,31 @@ package ex02_json_file;
 
 public class MainWrapper {
 
+  
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    
+    // 문제. 아래 내용을 가지는 product.json 파일 생성하기
+    /*
+        [ 
+          {
+            "code": "0001",    
+            "name": "냉장고",    
+            "price": 100,    
+            "sales": [
+              "하이마트",
+              "전자랜드" 
+             ]
+            },
+            {
+              "code": "0002",   
+              "name": "에어컨",    
+              "price": 200,    
+              "sales": [
+                "하이마트",
+             ]
+            }
+           ]
+     */
   }
 
 }
