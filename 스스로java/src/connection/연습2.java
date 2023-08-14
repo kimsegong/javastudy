5cf9d2c3e638731d040bf1af3c763f0e9b0d26b4
kimsegong <kimsegong@naver.com>
2023-08-14 19:49:19 +0900

    20230814

---
+++ 스스로java/src/connection/연습2.java
@@ -0,0 +1,19 @@
+package connection;
+
+public class 연습2 {
+
+  
+  public static void ex01() {
+    
+    
+    String age = "10";
+    int age1 = 10;
+    System.out.println(age);
+  }
+  
+  public static void main(String[] args) {
+   
+    ex01();
+  }
+
+}
