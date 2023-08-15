f4c5c02c51cfc1f026770a2d0c09a597abf9da6a
kimsegong <kimsegong@naver.com>
2023-08-15 15:31:22 +0900

    20230815

---
+++ 스스로java/src/connection/연습3.java
@@ -0,0 +1,25 @@
+package connection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 연습3 {
+public static void ex01() {
+  
+  List<String> ansml = new ArrayList<String>();
+  
+  ansml.add("apple");
+  ansml.add("banana");
+  ansml.add("strawberry");
+  
+  System.out.println(ansml);
+}
+  
+  
+  
+  public static void main(String[] args) {
+    
+    ex01();
+  }
+
+}
