531801658cafa5add01120011bedeea6afdef285
kimsegong <kimsegong@naver.com>
2023-08-17 20:42:36 +0900

    20230817

---
+++ 스스로java/src/connection/연습4.java
@@ -0,0 +1,14 @@
+package connection;
+
+public class 연습4 {
+
+  public static void main(String[] args) {
+   String name = "세연";
+   String mind = "성공하자";
+   String success = "성공했다";
+   
+   System.out.println(name + mind + success);
+
+  }
+
+}
