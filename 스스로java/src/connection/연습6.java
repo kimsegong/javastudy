0afccb22391751730dfa49249eb2c2c88d5d90a9
kimsegong <kimsegong@naver.com>
2023-08-27 09:11:33 +0900

    20230827

--- 스스로java/src/connection/연습6.java
+++ 스스로java/src/connection/연습6.java
@@ -4,4 +4,6 @@ public class 연습6 {
 
   String holiday = "토요일";
   int date = 26;
+  
+  String byebye ="코코";
 }
---
+++ 스스로java/src/connection/연습7.java
@@ -0,0 +1,11 @@
+package connection;
+
+public class 연습7 {
+
+  public static void main(String[] args) {
+    
+    String mon = "momo";
+
+  }
+
+}
